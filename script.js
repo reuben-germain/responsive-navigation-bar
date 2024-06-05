ade7d8fc20a12233059a5bd51aa2431043ac64e1
Reuben Germain <reuben.germain@soprasteria.com>
2024-06-05 12:41:09 +0100

    intial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('hamburger').addEventListener('click', function() {
+    var navLinks = document.getElementById('navLinks');
+    if (navLinks.style.display === 'flex') {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'flex';
+    }
+});
